9c6ea9ae424c629a30e53458b79c7e6239e9adcc
kaushalkrishna011 <kaushalkrishna011@gmail.com>
2025-08-24 11:47:01 +0000

    Fix: import issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import AppContext from "./context/items/AppContext";
 import UserContext from "./context/items/UserContext";
 import LoadingScreen from "./components/app/LoadingScreen";
